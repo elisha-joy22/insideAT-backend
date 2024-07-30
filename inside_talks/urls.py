9f9257e2a74b51e113c908d331ac616a4b187992
Elisha <elishagjoy98@gmail.com>
2024-07-30 11:21:53 +0530

    models,serialisers and urls added

--- inside_talks/urls.py
+++ inside_talks/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from inside_talks.views import GenreViewSet, VideoMetadataViewSet, VideoThumbnailViewSet
+
+
+router = DefaultRouter()
+router.register(r'genres', GenreViewSet)
+router.register(r'video_metadata', VideoMetadataViewSet)
+router.register(r'video_thumbnails', VideoThumbnailViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
